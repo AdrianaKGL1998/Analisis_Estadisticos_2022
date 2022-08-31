02570f9cdaf13e882169cf535140f063fd67a237
Adriana Garcia <k.reny120798@gmail.com>
2022-08-30 22:12:46 -0500

    TAREA 1 WORK

--- Tareas/Tarea_1.R
+++ Tareas/Tarea_1.R
@@ -1,4 +1,4 @@
-#Adriana Kareny García Ledezma
+#Adriana Kareny GarcÃ­a Ledezma
 # 28/08/2022
 # Tarea 1
 
@@ -12,23 +12,16 @@ Pinus <- 3140;Mezquite <- 1453;Encinos <- 450;
 Teka <- 1200;Juiperos <- 720;
 
 superficie <- c(Pinus,Mezquite,Encinos,Teka,Juiperos)
+s_creciente <- sort(superficie)
+s_decreciente <- sort(superficie,decreasing = T)
 
-#x11()
+barplot(s_creciente,main=" sort_Creciente"
+        ,ylab="Frecuencia",names.arg = c("Encinos","Juiperos","Teka","Mezquite","Pinus"),col = 'pink' )
+barplot(s_decreciente,main=" sort_Decreciente"
+        ,ylab="Frecuencia",names.arg = c("Pinus","Mezquite","Teka","Juiperos","Encinos"),col = 'blue' )
 barplot(superficie,main=" Superficies Reforestada"
         ,ylab="Frecuencia", names.arg = c("Pinus","Mezquite","Encinos","Teka","Juiperos"),col = rainbow(5))
 
-s_creciente <- sort(superficie)
-
-#x11()
-barplot(s_creciente,main=" sort_Creciente"
-        ,ylab="Frecuencia",names.arg = c("Encinos","Juiperos","Teka","Mezquite","Pinus"),col = 'pink' )
-
-
-s_decreciente <- sort(superficie,decreasing = T)
-
-#x11()
-barplot(s_decreciente,main=" sort_Decreciente"
-        ,ylab="Frecuencia",names.arg = c("Pinus","Mezquite","Teka","Juiperos","Encinos"),col = 'blue' )
 
 mean(superficie)
 
@@ -48,3 +41,4 @@ altura <- c(38, 14, 44, 11, 9, 21, 39, 28, 41, 4, 35, 24, 36, 12,
             20, 31, 24, 25, 10, 21, 11, 36, 37, 20, 26)
 mean(altura)
 sd(altura)
+
